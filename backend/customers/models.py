6bd1eb39f0fa4143ccb6267e1c4e13322ad313fe
Quang Nguyen <qmnguyenw@gmail.com>
2020-03-04 10:57:27 +0700

    setting up project

--- backend/customers/models.py
+++ backend/customers/models.py
@@ -5,3 +5,6 @@ class Customer(models.Model):
     name = models.CharField(max_length=70, blank=False, default='')
     age = models.IntegerField(blank=False, default=1)
     active = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
